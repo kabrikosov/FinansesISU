74370c90af0643b9100af7f978f52eca5968c65f
kelogub <belogub.konstantin200202@yandex.ru>
2022-05-18 23:13:21 +0800

    done react js pie chart

--- src/main/client/js/app.js
+++ src/main/client/js/app.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import {Chart} from "react-google-charts";
+
+const monthNames = {
+    en: ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"],
+    ru: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+        "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+};
 
 class App extends React.Component {
 
@@ -31,10 +37,66 @@ class FinanceCircle extends React.Component {
         };
     }
 
+    updateDate(date){
+        this.setState(prevState => ({
+            date: date,
+            statistics: prevState.statistics,
+            options: prevState.options
+        }));
+    }
+
     componentDidMount() {
-        var requ = window.location.origin + "/api/statistics/sumGroup?start=" +
-            new Date(this.state.date.getFullYear(), this.state.date.getMonth(), 0).toISOString().substring(0, 10) +
-            "&end=" + new Date(this.state.date.getFullYear(), this.state.date.getMonth() + 1, 0).toISOString().substring(0, 10);
+        this.initializeChartPie();
+        this.render();
+    }
+
+    render() {
+        return (
+            <div className="finance-circle__wrapper">
+                <div className="finance-circle__head">
+                    <img className="arrow arrow--finance-previous-month" onClick={this.previousMonth()}
+                         src="http://localhost:8080/resources/img/arrow.svg"
+                         alt="previous month" id="previous-month"/>
+                    <span className="finance-circle__month" id="finance-circle__month"
+                    >{monthNames[lang][this.state.date.getMonth()] + " " + this.state.date.getFullYear()}</span>
+                    <img className="arrow arrow--finance-next-month"
+                         src="http://localhost:8080/resources/img/arrow.svg"
+                         alt="next month" id="next-month" onClick={this.nextMonth()}/>
+                </div>
+                <div className="finance-circle">
+                    <div id="piechart"></div>
+                </div>
+            </div>
+        )
+    }
+
+    previousMonth() {
+        return (ev) => {
+            let date = new Date(this.state.date.getFullYear(), this.state.date.getMonth() - 1, this.state.date.getDay());
+            this.updateDate(date)
+            document.getElementById("finance-circle__month").textContent = monthNames[lang][this.state.date.getMonth()] + " " + this.state.date.getFullYear();
+            this.initializeChartPie(date);
+            this.render();
+        };
+    }
+
+    nextMonth() {
+        return (ev) => {
+            let date = new Date(this.state.date.getFullYear(), this.state.date.getMonth() + 1, this.state.date.getDay())
+            this.updateDate(date)
+            document.getElementById("finance-circle__month").textContent = monthNames[lang][this.state.date.getMonth()] + " " + this.state.date.getFullYear();
+            this.initializeChartPie(date);
+            this.render();
+        }
+    }
+
+    initializeChartPie(date=null) {
+        if (date == null){
+            date = this.state.date;
+        }
+        let requ = window.location.origin + "/api/statistics/sumGroup?start=" +
+            new Date(date.getFullYear(), date.getMonth(), 0).toISOString().substring(0, 10) +
+            "&end=" + new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().substring(0, 10);
 
         fetch(requ)
             .then((response) => {
@@ -50,39 +112,15 @@ class FinanceCircle extends React.Component {
                         options: prevState.options
                     })
                 );
-                this.render();
-            });
-    }
+                google.charts.load('current', {'packages': ['corechart']});
+                google.charts.setOnLoadCallback(ev => {
+                    let data = google.visualization.arrayToDataTable(loaded);
 
-    render() {
-        return (
-            <div className="finance-circle__wrapper">
-                <div className="finance-circle__head">
-                    <img className="arrow arrow--finance-previous-month"
-                         src="http://localhost:8080/resources/img/arrow.svg"
-                         alt="previous month" id="previous-month"/>
-                    <span className="finance-circle__month" id="finance-circle__month"
-                    >{this.state.date.toISOString().substring(0, 7)}</span>
-                    <img className="arrow arrow--finance-next-month"
-                         src="http://localhost:8080/resources/img/arrow.svg"
-                         alt="next month" id="next-month"/>
-                </div>
-                <div className="finance-circle">
-                    <Chart
-                        chartType="PieChart"
-                        data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-                        options = {{
-                            title: "My Daily Activities",
-                            pieHole: 0.4,
-                            is3D: false,
-                        }}
-                        width="100%"
-                        height="400px"
-                        legendToggle
-                    />
-                </div>
-            </div>
-        )
+                    // Display the chart inside the <div> element with id="piechart"
+                    let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                    chart.draw(data, this.state.options);
+                });
+            });
     }
 }
 
