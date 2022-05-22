c9b12a30a554d848446ae561d272289442e6da97
kelogub <belogub.konstantin200202@yandex.ru>
2022-05-22 19:37:10 +0800

    Query for getting sum of all the subscribed products and grouping

--- src/main/java/ru/isu/repository/SubscriptionRepository.java
+++ src/main/java/ru/isu/repository/SubscriptionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.isu.model.Subscription;
+import ru.isu.projection.GrouppingSumProjection;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,8 +20,14 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Inte
             "or (s.startDateTime < :dateStart and (s.expirationDateTime is null or s.expirationDateTime >= :dateEnd))")
     Integer findSumByDateBetween(@Param("dateStart") LocalDateTime dateStart, @Param("dateEnd") LocalDateTime dateEnd);
 
-    @Query("from Subscription s where (s.startDateTime between :dateStart and :dateEnd) or (s.startDateTime <= :dateStart and (s.expirationDateTime is null or s.expirationDateTime >= :dateStart))")
+    @Query("select s from Subscription s where (s.startDateTime between :dateStart and :dateEnd) " +
+            "or (s.startDateTime <= :dateStart and (s.expirationDateTime is null or s.expirationDateTime >= :dateStart))")
     List<Subscription> findSubscriptionsByDates(@Param("dateStart") LocalDateTime dateStart, @Param("dateEnd") LocalDateTime dateEnd);
+
+    @Query("select s.id as id, sum(p.price*p.quantity) as sum from Product p join Subscription s on p.subscription = s where (s.startDateTime between :dateStart and :dateEnd) " +
+            "or (s.startDateTime <= :dateStart and (s.expirationDateTime is null or s.expirationDateTime >= :dateStart)) group by s")
+    List<GrouppingSumProjection> findSubscriptionsSumGroupping(@Param("dateStart") LocalDateTime dateStart, @Param("dateEnd") LocalDateTime dateEnd);
+
 //    List<SubscriptionInfo> findSubscriptionsByStartDateTimeBeforeAndAndExpirationDateTimeAfterOrStartDateTimeBeforeAndExpirationDateTimeIsNull(LocalDateTime startDateTime, LocalDateTime expirationDateTime);
 
 }
